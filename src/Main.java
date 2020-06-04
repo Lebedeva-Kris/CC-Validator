4f16d242e2428390c47673dad1e5f6ef96372006
Kristina <kris2015.lebedeva@yandex.ru>
2020-06-04 23:59:24 +0300

    InstaPayment accepted

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "4175006486174384";
+        String number = "6396417728526224";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
