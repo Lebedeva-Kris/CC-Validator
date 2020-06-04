cc716123c67e57589c2eac6c2d8002f103ef11f5
Kristina <kris2015.lebedeva@yandex.ru>
2020-06-04 23:41:48 +0300

    MC accepted

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "4024007114667958";
+        String number = "5528776724665085";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
